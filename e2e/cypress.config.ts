805c66c75d5a1a70bb610fe032a2bf880ee86581
yukimaru5447 <83536844+yukimaru5447@users.noreply.github.com>
2022-06-26 21:07:10 +0900

    テストレポート出力

--- e2e/cypress.config.ts
+++ e2e/cypress.config.ts
@@ -6,11 +6,22 @@ export default defineConfig({
       // implement node event listeners here
     },
   },
-
-  component: {
-    devServer: {
-      framework: "create-react-app",
-      bundler: "webpack",
-    },
+  reporter: "mochawesome",
+  reporterOptions: {
+    reportDir: "cypress/results",
+    overwrite: false,
+    html: false,
+    json: true,
   },
+  // reporter: "junit",
+  // reporterOptions: {
+  //   mochaFile: "results/my-test-output.xml",
+  //   toConsole: true,
+  // },
+  // component: {
+  //   devServer: {
+  //     framework: "create-react-app",
+  //     bundler: "webpack",
+  //   },
+  // },
 });
